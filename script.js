ab64bbf7d46cadcd9664102a31bdcf9c05f01109
JTylerH <JTylerH@users.noreply.github.com>
2021-07-04 19:06:55 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    // $('.nav-container').remove();
+    // $('#toggle-interactions').on('click', function() {
+    //     $("#interactions-container").html('<iframe src="https://www.sli.do/></iframe>"');
+    // });
+    $('.sc-emDrdl.jtIXSk').html('<iframe src="https://wave.church/forms/cg" style="height:200px" frameborder="0"></iframe>');
+    $('.video').css({"width": "calc(100% - 700px)"});
+    $('.video').before('<div class="poll-panel" style="width: 292px;"><div style="height:100%"><iframe src="https://app.sli.do/event/33iiv0e2" height="100%" width="100%" frameBorder="0" style="min-height: 560px;" title="Slido"></iframe></div></div>')
+});
