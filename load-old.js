7c72a2f97659427bf1285db10c85b08024f65137
JTylerH <JTylerH@users.noreply.github.com>
2021-07-26 18:45:43 -0400

    Create load-old.js

---
+++ load-old.js
@@ -0,0 +1,13 @@
+var j = document.createElement('script');
+j.setAttribute('src','https://code.jquery.com/jquery-3.6.0.min.js');
+j.setAttribute('integrity','sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=');
+j.setAttribute('crossorigin','anonymous');
+document.body.appendChild(j);
+var s = document.createElement('script');
+s.setAttribute('src','https://cdn.jsdelivr.net/gh/JTylerH/brushfire/script.js');
+document.body.appendChild(s);
+var c = document.createElement('link');
+c.setAttribute('rel','stylesheet');
+c.setAttribute('rel','stylesheet');
+c.setAttribute('href','https://cdn.jsdelivr.net/gh/JTylerH/brushfire/style.css');
+document.body.appendChild(c);
